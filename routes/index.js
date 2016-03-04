bc795bb559628ad19b789451e795c604133b112d
amdudda <tk0654wm@minneapolis.edu>
2016-03-03 20:22:08 -0600

    add basic error trapping

--- routes/index.js
+++ routes/index.js
@@ -6,27 +6,50 @@ var request_mod = require("request");
 router.get('/',homepage);
 
 function homepage(req, res) {
-	// TODO: the api has a call that lets you get all available currencies.
+	// The api has a call that gets a list of all available currencies.
 	// this could be used to generate massive drop-down boxes.  
 	var api_url = "http://apilayer.net/api/list";	// url for list of currencies
 	var apikey = process.env.CURRENCYLAYER_API_KEY;  // fetch my api key
-	var api_param = { 'access_key':apikey } ;
+	var api_param = { 'access_key':apikey } ;  // and set it as a parameter for the call
 
 	request_mod( {uri: api_url, qs: api_param}, function(error,clresponse,body) {
 		if (!error && clresponse.statusCode == 200) {
 			// parse out the currencies and loop through them to create options
 			var my_data = JSON.parse(body);
-			var my_currencies = my_data.currencies;
-			var currency_options = ""
-			for (var currency in my_currencies) {
-				console.log(currency + "... " + my_currencies[currency]);
-				currency_options += "| <option value='" + currency + "'> " + my_currencies[currency] + "</option>\n";
-			}
-			res.render('index',{currencies_list:currency_options});  // this renders the named view (index.jade)
+			if (my_data.success) {
+				var my_currencies = my_data.currencies;
+				var currency_options = "";
+				for (var currency in my_currencies) {
+					currency_options += "| <option value='" + currency + "'> " + my_currencies[currency] + "</option>\n";
+				}
+				// render the named view (index.jade) and pass my list of available currencies to the page.
+				res.render('index',{currencies_list:currency_options});  
+			} else {
+				// process error info in JSON data
+				var errcode = my_data.error.code;
+				var errtype = my_data.error.type;
+				var errtext = my_data.error.info;
+				// and pass this to an error page
+				res.render('error',{errcode:errcode,errtype:errtype,errtext:errtext});
+			} // end if-else for processing JSON data
+		}
+		else if (!error) {
+			// fall back in a more appropriate fashion - tell the user something
+			// went wrong in the HTML request.
+			if (clresponse) var errcode = clresponse.statusCode;
+				else var errcode = "Unable to retrieve error code";
+			var errtype = "HTML error";
+			var errtext = "Something happened in communications between your computer and the server on the Internet.  You can do a web search on the error code above to find more details about the error.";
+			// and pass this to an error page
+			res.render('error',{errcode:errcode,errtype:errtype,errtext:errtext});
 		}
 		else {
-			// TODO some sort of error occurred, send user to error page
-		}
+			var errcode = "Unable to retrieve error code";
+			var errtype = "Error in program";
+			var errtext = "Something broke in the code.  Nag me at tk0654wm@go.minneapolis.edu to fix it.";
+			// and pass this to an error page
+			res.render('error',{errcode:errcode,errtype:errtype,errtext:errtext});
+		} // end if-else for processing request_mod response
 	}); // end request callback
 } // end homepage function
 
@@ -46,11 +69,11 @@ function convert(req, res) {
 	var toSymbol="";
 	var fromSymbol="";
 	// pre-staging for introducing other currencies
-	if (convertTo == "EUR") toSymbol = "€ ";
+	if (convertTo == "EUR") toSymbol = "€";
 		else if (convertTo == "USD") toSymbol = "US$";
 		else if (convertTo == "GBP") toSymbol = "GB₤"; 
 		else toSymbol = convertTo;
-	if (convertFrom == "EUR") fromSymbol = "€ "; 
+	if (convertFrom == "EUR") fromSymbol = "€"; 
 		else if (convertFrom == "USD") fromSymbol = "US$";
 		else if (convertFrom == "GBP") fromSymbol = "GB₤"; 
 		else fromSymbol = convertFrom;
@@ -76,10 +99,10 @@ function convert(req, res) {
 	// our conversion rates generated via api call
 	request_mod( {uri: api_url, qs: api_params}, function(error,clresponse,body) {
 		if (!error && clresponse.statusCode == 200) {
-			var api_conversions = JSON.parse(body); // how to fetch this??
+			var api_conversions = JSON.parse(body); 
 			// check for data retrieval success
 			if (api_conversions.success) {
-				console.log(api_conversions.quotes);
+				//console.log(api_conversions.quotes);
 				var target = api_conversions["quotes"]["USD" + convertTo];
 				var base = api_conversions["quotes"]["USD" + convertFrom];
 				convertedVal = units * (target/base);
@@ -105,8 +128,7 @@ function convert(req, res) {
 			res.render('error',{errcode:errcode,errtype:errtype,errtext:errtext});
 		}
 		else {
-			if (clresponse) var errcode = clresponse.statusCode;
-				else var errcode = "Unable to retrieve error code";
+			var errcode = "Unable to retrieve error code";
 			var errtype = "Error in program";
 			var errtext = "Something broke in the code.  Nag me at tk0654wm@go.minneapolis.edu to fix it.";
 			// and pass this to an error page
